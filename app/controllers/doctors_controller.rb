240555fef9398b848924cf6f529f3c367ab5e7f0
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-26 17:10:32 -0700

    index show new patients

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -12,7 +12,7 @@ class DoctorsController < ApplicationController
     @doctor = Doctor.new(doctor_params)
 
     if @doctor.save
-      redirect_to @doctor
+      redirect_to @doctor #doctor_path(:id)
     else
       render :new
     end
@@ -23,10 +23,13 @@ class DoctorsController < ApplicationController
   end
 
   def edit
+    #use _form but in this case we only have 1 parameter of last_name so it's not super important to edit right now
   end
 
   def destroy
     #BEFORE_ACTION
+    @doctor.destroy
+    redirect_to root_path
   end
 
 private
--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,13 +1,43 @@
 class PatientsController < ApplicationController
+  before_action :set_patient, only: [:show, :destroy]
   def index
+    @patients = Patient.all
   end
 
   def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.new
+
+    if @patient.save
+      redirect_to patients_path
+    else
+      render :new
+    end
   end
 
   def show
+    #BEFORE_ACTION
   end
 
   def edit
+    #same as doctors_controller, add this action in later
+  end
+
+  def destroy
+    @patient.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.require(:patient).permit(:first_name,:last_name)
   end
 end
